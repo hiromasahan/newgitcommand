baae285eb4a4a1a70b0e77ca6d55028b18b2c8e5
hiromasahan <hiromasahan@163.com>
2014-08-09 19:46:52 +0800

    the second example of template1

--- Lesson01/Lesson01.js
+++ Lesson01/Lesson01.js
@@ -1,6 +1,17 @@
 if (Meteor.isClient) {
-  Template.person.fullname = "Joe Smith";
-  Template.person.job = "Developer";
-  Template.person.likes = ["bananas","JavaScript","cars"]
-}
 
+
+  Template.staff.people = [
+     {  fullName: "John Doe", job: "CEO"  },
+     {  fullName: "Jane Smith", job: "CTO"  },
+     {  fullName: "Sophie Turner", job: "Developer"  },
+     {  fullName: "Jack Lewis", job: "Designer"  },
+  ];
+
+
+  Template.person.executive = fuction () {
+     return !!this.job.match(/^C.*O$/);
+
+  };
+}
+  
