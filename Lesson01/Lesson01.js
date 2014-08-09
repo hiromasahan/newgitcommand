06e4f4e571e6e184228d59f4ea1c32f01272eb1f
hiromasahan <hiromasahan@163.com>
2014-08-09 18:58:01 +0800

    first example of People List

--- Lesson01/Lesson01.js
+++ Lesson01/Lesson01.js
@@ -1,19 +1,6 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to Lesson01.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  Template.person.fullname = "Joe Smith";
+  Template.person.job = "Developer";
+  Template.person.likes = ["bananas","JavaScript","cars"]
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
