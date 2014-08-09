d69f0a166ee29309d700c165fe7d2d5c61a78f16
hiromasahan <hiromasahan@163.com>
2014-08-09 21:44:28 +0800

    the third example in template2

--- Lesson02/Lesson02.js
+++ Lesson02/Lesson02.js
@@ -1,10 +1,28 @@
+
+function add(item) {
+   var list = Session.get("list");
+   list.push(item);
+   Session.set("list",list)
+
+}
+
 if (Meteor.isClient) {
-
-
    Session.set("list",["one", "two"]);
    
   Template.list.items = function () {
    return Session.get("list");
   };
+
+  Template.list.events({
+     'keypress input': function(e,t){
+      if(e.keyCode === 13){
+        var input = t.find("input");
+        add(input.value);
+        input.value = "";
+       }
+
+     }
+
+  });
 }
 
