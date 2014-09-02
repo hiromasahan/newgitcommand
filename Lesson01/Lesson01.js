2baf1d918af0d6d4f16b0ddba966a657e90cf73f
hiromasahan <hiromasahan@163.com>
2014-09-02 14:06:38 +0800

    can not tell the differences between the Client & the Server in console.log

--- Lesson01/Lesson01.js
+++ Lesson01/Lesson01.js
@@ -1,5 +1,7 @@
-if (Meteor.isClient) {
 
+
+if (Meteor.isClient) {
+console.log("hello world")
 
   Template.staff.people = [
      {  fullName: "John Doe", job: "CEO"  },
@@ -13,5 +15,8 @@ if (Meteor.isClient) {
      return !!this.job.match(/^C.*O$/);
 
   };
+}
+  if (Meteor.isServer) {
+console.log("hello world")
 }
   
